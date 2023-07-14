5466895e58142d49fc993eecc731d42b5c5d31c6
lucas-goldner <lucas.goldner@googlemail.com>
2023-07-14 20:22:01 +0200

    doc: Add documentation for models

--- lib/components/model/animation_arguments.dart
+++ lib/components/model/animation_arguments.dart
@@ -2,7 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:fluttershow_base/components/model/enum/animations.dart';
 import 'package:fluttershow_base/components/model/enum/directional_animation_direction.dart';
 
+/// A class used to define which animation should be used,
+///  with additional properties.
 class AnimationArguments {
+  /// Constructs an AnimationArguments object.
+  ///
+  /// [animation] specifies the type of animation.
+  ///
+  /// [delay] is the duration (in milliseconds) to delay the animation.
+  ///
+  /// [animationCurve] is an optional parameter that specifies the curve
+  /// used for the animation. If not provided,
+  /// the default curve is Curves.easeIn.
+  ///
+  /// [direction] is an optional parameter that specifies the direction of
+  /// a directional animation. It is required when the animation is
+  /// Animations.directionalAnimation.
   AnimationArguments(
     this.animation, {
     required this.delay,
@@ -14,8 +29,30 @@ class AnimationArguments {
           'For Animations.directionalAnimation, a direction is required',
         );
 
+  /// The type of animation.
+  ///
+  /// Example:
+  /// ```
+  /// Animations.fadeIn
+  /// ```
   final Animations animation;
+
+  /// The duration (in milliseconds) to delay the animation.
   final int delay;
+
+  /// The curve used for the animation.
+  ///
+  /// Example:
+  /// ```
+  /// Curves.easeIn
+  /// ```
   final Curve curve;
+
+  /// The direction of a directional animation.
+  ///
+  /// Example:
+  /// ```
+  /// DirectionalAnimationDirection.left
+  /// ```
   final DirectionalAnimationDirection? direction;
 }
--- lib/components/model/enum/list_bullets.dart
+++ lib/components/model/enum/list_bullets.dart
@@ -1,12 +1,28 @@
+/// An enum representing different types of bullets for lists.
 enum ListBullets {
+  /// Represents a circle bullet •.
   circle('\u2022'),
-  circle_big('\u25CF'),
-  circle_outline('\u25E6'),
+
+  /// Represents a big circle bullet ●.
+  circleBig('\u25CF'),
+
+  /// Represents a circle outline bullet ◦.
+  circleOutline('\u25E6'),
+
+  /// Represents a square bullet ▪.
   square('\u25AA'),
-  square_mini('\u2219'),
+
+  /// Represents a mini square bullet ∙.
+  squareMini('\u2219'),
+
+  /// Represents a triangle bullet ‣.
   triangle('\u2023');
 
+  /// Constructs a ListBullets object with the specified unicode character.
+  ///
+  /// [uniCode] is the unicode character associated with the bullet.
   const ListBullets(this.uniCode);
 
+  /// The unicode character associated with the bullet.
   final String uniCode;
 }
